ce8ea09ee851c9e0381964d9e96a278ea134c709
Adrian Jaroszuk <adrianjaroszuk571@gmail.com>
2022-10-26 02:15:00 +0200

    Added functions to JS

--- script.js
+++ script.js
@@ -1,39 +1,41 @@
-let number1Element = document.querySelector(".js-number1")
-let number2Element = document.querySelector(".js-number2")
-let courseElement = document.querySelector(".js-course")
-let buttonElement = document.querySelector(".js-button")
-let formElement = document.querySelector(".js-form")
-let currencyFrom = document.querySelector(".currency1")
-let currencyTo = document.querySelector(".currency2")
-    
 
-formElement.addEventListener("input", (event) => {
-    event.preventDefault();
+{
+    const number1Element = document.querySelector(".js-number1")
+    const courseElement = document.querySelector(".js-course")
+    const number2Element = document.querySelector(".js-number2")
+    const formElement = document.querySelector(".js-form")
 
-    let number1 = number1Element.value;
-    let course = courseElement.value;
+    formElement.addEventListener("input", (event) => {
+        event.preventDefault();
 
-    // rate conversion
+        const number1 = number1Element.value;
+        const course = courseElement.value;
 
-    let number2 = number1 * course;
+        const number2 = number1 * course;
 
-    document.getElementById("number2").value = number2.toFixed(2);
-})
+        document.getElementById("number2").value = number2.toFixed(2);
+    })
+}
 
-//Change of currency and value rate
+{
 
-buttonElement.addEventListener("click", () => {
-    currencyFrom.classList.toggle("js-EUR");
-    number2.innerText = "";
+    const buttonElement = document.querySelector(".js-button")
+    const currencyFrom = document.querySelector(".currency1")
+    const currencyTo = document.querySelector(".currency2")
 
-    if (currencyFrom.classList.contains("js-EUR")) {
-        currencyFrom.innerText = "PLN:";
-        currencyTo.innerText = "EUR:";
-        document.getElementById("course").value = 0.21
-    } else {
-        currencyFrom.innerText = "EUR:";
-        currencyTo.innerText = "PLN:";
-        document.getElementById("course").value = 4.81
-    }
+    buttonElement.addEventListener("click", () => {
+        currencyFrom.classList.toggle("js-EUR");
+        number2.innerText = "";
 
-});
+        if (currencyFrom.classList.contains("js-EUR")) {
+            currencyFrom.innerText = "PLN:";
+            currencyTo.innerText = "EUR:";
+            document.getElementById("course").value = 0.21
+        } else {
+            currencyFrom.innerText = "EUR:";
+            currencyTo.innerText = "PLN:";
+            document.getElementById("course").value = 4.81
+        }
+
+    });
+}
